f3d3fc0a66ec3280c85969149781227160bd3e09
Andrew Golightly <support@andrewgolightly.com>
2013-12-05 22:48:19 +0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var numerologyApp = angular.module('numerologyApp', [
+    'ngRoute',
+    'numerologyControllers'
+]);
+
+numerologyApp.config(['$routeProvider',
+                    function($routeProvider) {
+                        $routeProvider.
+                        when('/numerologyCalculator', {
+                            templateUrl: 'partials/numerology-client-details.html',
+                            controller: 'NumerologyClientInfoCtrl'
+                        }).
+                        when('/numerologyCalculator/lifepath/:lifepathNumber', {
+                            templateUrl: 'partials/numerology-lifepath.html',
+                            controller: 'NumerologyLifepathCtrl'
+                        }).
+                        otherwise({
+                            redirectTo: '/numerologyCalculator'
+                        });
+                    }]);
---
+++ js/controllers.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/* Controllers */
+
+var numerologyControllers = angular.module('numerologyControllers', []);
+
+numerologyControllers.controller('NumerologyClientInfoCtrl', function($scope, $location) {
+    $scope.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    
+    $scope.submitClientDetails = function() {
+        
+        $location.path("/numerologyCalculator/lifepath/" + calcLifePathNumber());
+    };
+    
+    function calcLifePathNumber() {
+        return 8;
+    }
+});
+
+numerologyControllers.controller('NumerologyLifepathCtrl',
+                                 ['$scope', '$routeParams', '$http', 
+                                 
+                                  function($scope, $routeParams, $http) {
+                                    
+                                      /*
+                                      $http.get('/meanings/lifepath/' + $routeParams.lifepathNumber + '.html').success(function(data) {
+                                          $scope.lifepathContent = data;
+                                      });
+                                      */
+                                      
+                                      
+                                      $scope.lifepathNumber = $routeParams.lifepathNumber;
+                                      $scope.lifepathURL = '/meanings/lifepath/' + $routeParams.lifepathNumber + '.html';
+                                      $scope.lifepathStuff = "<strong>this is fun</strong>";
+                                  }]);
